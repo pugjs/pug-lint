c0a6c131fe056a90d8d28fc3361ae9f37eab18ac
Ben Edwards <ben.edwards@clock.co.uk>
2015-07-09 20:06:49 +0100

    > Refactor linter

---
+++ test/linter.test.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+  , Linter = require('../lib/linter')
+
+describe('linter', function () {
+
+  var linter
+
+  describe('config', function () {
+
+    it('should have no default configured rules', function () {
+      linter = new Linter()
+      linter.configure()
+
+      assert.equal(linter.getConfiguredRules().length, 0)
+    })
+
+  })
+
+})
