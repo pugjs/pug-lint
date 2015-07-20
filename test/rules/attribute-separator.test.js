b83a23dcd067239e8f25252dc20ac94bcc88e861
Ben Edwards <ben.edwards@clock.co.uk>
2015-07-20 16:27:14 +0100

    > Refactor attribute separator rule

---
+++ test/rules/attribute-separator.test.js
@@ -0,0 +1,101 @@
+module.exports = createTest
+
+var assert = require('assert')
+
+function createTest(linter) {
+
+  describe('attributeSeparator', function () {
+
+    describe('null', function () {
+
+      it('should be disabled if options are null', function () {
+        linter.configure({ attributeSeparator: null })
+
+        assert.equal(linter.getConfiguredRules().length, 0)
+      })
+
+    })
+
+    describe('space', function () {
+
+      before(function () {
+        linter.configure({ attributeSeparator: ' ' })
+      })
+
+      it('should report invalid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\', name=\'name\', value=\'value\')').length, 1)
+      })
+
+      it('should not report valid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' name=\'name\' value=\'value\')').length, 0)
+      })
+
+    })
+
+    describe('comma', function () {
+
+      before(function () {
+        linter.configure({ attributeSeparator: ',' })
+      })
+
+      it('should report invalid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' name=\'name\' value=\'value\')').length, 1)
+      })
+
+      it('should not report valid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\',name=\'name\',value=\'value\')').length, 0)
+      })
+
+    })
+
+    describe('comma-space', function () {
+
+      before(function () {
+        linter.configure({ attributeSeparator: ', ' })
+      })
+
+      it('should report invalid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' name=\'name\' value=\'value\')').length, 1)
+      })
+
+      it('should not report valid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\', name=\'name\', value=\'value\')').length, 0)
+      })
+
+    })
+
+    describe('space-comma', function () {
+
+      before(function () {
+        linter.configure({ attributeSeparator: ' ,' })
+      })
+
+      it('should report invalid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\', name=\'name\', value=\'value\')').length, 1)
+      })
+
+      it('should not report valid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' ,name=\'name\' ,value=\'value\')').length, 0)
+      })
+
+    })
+
+    describe('space-comma-space', function () {
+
+      before(function () {
+        linter.configure({ attributeSeparator: ' , ' })
+      })
+
+      it('should report invalid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' name=\'name\' value=\'value\')').length, 1)
+      })
+
+      it('should not report valid attribute separator', function () {
+        assert.equal(linter.checkString('input(type=\'text\' , name=\'name\' , value=\'value\')').length, 0)
+      })
+
+    })
+
+  })
+
+}
