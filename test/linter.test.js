abd6dea19b823be6f0fb156aed292492467f3b8b
Ben Edwards <ben.edwards@clock.co.uk>
2015-07-23 17:26:03 +0100

    > Don't check empty strings

--- test/linter.test.js
+++ test/linter.test.js
@@ -3,22 +3,22 @@ var assert = require('assert')
 
 describe('linter', function () {
 
-  var linter
+  var linter = new Linter()
     , fixturesPath = __dirname + '/fixtures/'
 
   describe('configure', function () {
 
     it('should have no default configured rules', function () {
-      linter = new Linter()
       linter.configure()
 
       assert.equal(linter.getConfiguredRules().length, 0)
     })
 
-    it('should report errors during parsing', function () {
-      linter = new Linter()
-      linter.configure()
+    it('should no check empty strings', function () {
+      assert.equal(linter.checkString('').getErrorCount(), 0)
+    })
 
+    it('should report errors during parsing', function () {
       var result = linter.checkFile(fixturesPath + 'invalid.jade')
 
       assert.equal(result.getErrorCount(), 1)
