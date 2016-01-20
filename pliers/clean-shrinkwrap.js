fdda0377807aef6263d3db066b518924df709d1f
Ben Edwards <ben.edwards@clock.co.uk>
2016-01-20 16:28:25 +0000

    > Removes shrinkwrap

--- pliers/clean-shrinkwrap.js
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore next */
-module.exports = createTask
-
-/* istanbul ignore next */
-function createTask (pliers) {
-
-  pliers('cleanShrinkwrap', require('pliers-clean-shrinkwrap')(pliers))
-
-}
