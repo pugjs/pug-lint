d7ff37e8f16f46ab7cc282f9b594310b8d141d69
Ben Edwards <ben.edwards@clock.co.uk>
2016-05-25 12:26:38 +0100

    > Adds additional reporter test

--- test/reporters.test.js
+++ test/reporters.test.js
@@ -6,7 +6,11 @@ describe('reporters', function () {
   var linter = new Linter()
     , tests = []
 
-  linter.configure({ disallowBlockExpansion: true })
+  linter.configure(
+    { disallowBlockExpansion: true
+    , disallowMultipleLineBreaks: true
+    }
+  )
 
   glob.sync(__dirname + '/reporters/*.test.js').forEach(function (file) {
     tests.push(require(file))
