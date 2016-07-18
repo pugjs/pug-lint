f4203547bc704ba5770f9135583436d6ad5e8a88
Timothy Gu <timothygu99@gmail.com>
2016-07-18 08:12:12 +0800

    Add yeoman generator for rules (#97)

---
+++ generators/rule-templates/rule.js
@@ -0,0 +1,38 @@
+// # <%- optionName %>: `true`
+//
+// The Pug template must <%- requirement %>.
+//
+// ```pug
+// //- Invalid
+// bleh...
+//
+// //- Valid
+// blah...
+// ```
+
+<% if (optionsType !== 'other') { -%>
+var utils = require('../utils');
+<% } else { -%>
+var assert = require('assert');
+<% } -%>
+
+module.exports = function () {};
+
+module.exports.prototype = {
+  name: '<%- optionName %>',
+
+  configure: function (options) {
+<% if (optionsType === 'true') { -%>
+    utils.validateTrueOptions(this.name, options);
+<% } else if (optionsType === 'true|codeSep'){ -%>
+    utils.validateCodeOperatorOptions(this.name, options);
+<% } else { -%>
+    // reminder: possible options: <%- options.join('|') %>
+    assert(..., this.name + ' option requires ...');
+<% } -%>
+  },
+
+  lint: function (file, errors) {
+    // ...
+  }
+};
