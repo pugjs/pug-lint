69794cfaf0e1b2d6e6b792b531f0e2e009f5e6be
Jason Allan <code@jasonderrythomas.com>
2021-11-30 14:32:18 +0000

    Update Mocha and Sinon

--- test/rules/disallow-legacy-mixin-call.test.js
+++ test/rules/disallow-legacy-mixin-call.test.js
@@ -4,39 +4,37 @@ const assert = require('assert');
 
 function createTest(linter, fixturesPath, test) {
 	describe('disallowLegacyMixinCall', () => {
-		describe(true, () => {
-			before(() => {
-				linter.configure({ disallowLegacyMixinCall: true });
-			});
+		before(() => {
+			linter.configure({ disallowLegacyMixinCall: true });
+		});
 
-			it('should report legacy mixin call', () => {
-				test('mixin myMixin(variable, 2)', 1, 1);
-				test('div\n  mixin myMixin(variable, 2)\n  p Nay', 2, 3);
-			});
+		it('should report legacy mixin call', () => {
+			test('mixin myMixin(variable, 2)', 1, 1);
+			test('div\n  mixin myMixin(variable, 2)\n  p Nay', 2, 3);
+		});
 
-			it('should not report mixin definition', () => {
-				test('mixin myMixin(variable)\n  p Yay');
-				test('div\n  mixin myMixin(variable)\n    p Yay');
-			});
+		it('should not report mixin definition', () => {
+			test('mixin myMixin(variable)\n  p Yay');
+			test('div\n  mixin myMixin(variable)\n    p Yay');
+		});
 
-			it('should not report modern mixin call', () => {
-				test('+myMixin(variable, 2)');
-				test('+myMixin(variable, 2)\n  p Yay');
-			});
+		it('should not report modern mixin call', () => {
+			test('+myMixin(variable, 2)');
+			test('+myMixin(variable, 2)\n  p Yay');
+		});
 
-			it('should report multiple errors found in file', () => {
-				const result = linter.checkFile(
-					fixturesPath + 'disallow-legacy-mixin-call.pug'
-				);
+		it('should report multiple errors found in file', () => {
+			const result = linter.checkFile(
+				fixturesPath + 'disallow-legacy-mixin-call.pug'
+			);
 
-				assert.equal(result.length, 2);
-				assert.equal(
-					result[0].code,
-					'PUG:LINT_DISALLOWLEGACYMIXINCALL'
-				);
-				assert.equal(result[0].line, 3);
-				assert.equal(result[0].column, 1);
-			});
+			assert.equal(result.length, 2);
+			assert.equal(
+				result[0].code,
+				'PUG:LINT_DISALLOWLEGACYMIXINCALL'
+			);
+			assert.equal(result[0].line, 3);
+			assert.equal(result[0].column, 1);
 		});
 	});
 }
